8d084bc2c15b81540e3ab3d09672b9a9afcdb53f
Pavel Janíček <pavel.janicek@live.com>
2026-01-26 13:12:16 +0100

    add config

---
+++ config.php
@@ -0,0 +1,10 @@
+<?php
+// Block direct access
+if (!defined('ALLOW_ACCESS')) {
+    http_response_code(403); // Forbidden
+    exit('Access denied.');
+}
+
+// Sem pridej realny kody.
+$bot_token = "";
+$admin_id = "";
