956257050bec6f3f9c6f42b63b479e4f1a337b31
Pavel Janíček <pavel.janicek@live.com>
2026-01-26 14:27:53 +0100

    add config for mysql

--- config.php
+++ config.php
@@ -6,4 +6,12 @@ if (!defined('ALLOW_ACCESS')) {
 }
 
 // Sem pridej realny kody.
-$BOT_TOKEN = "";
+$BOT_TOKEN = "SEM_DEJ_TOKEN";
+
+// Database configuration
+$db_config = [
+    'host' => 'localhost',
+    'dbname' => 'dbname',
+    'user' => 'dbuser',
+    'password' => 'dbpassword'
+];
