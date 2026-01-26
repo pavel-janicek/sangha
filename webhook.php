59fc3739079a337b28b763fe7bc20f917b15f963
Pavel Janíček <pavel.janicek@live.com>
2026-01-26 16:08:23 +0100

    another name test

--- webhook.php
+++ webhook.php
@@ -67,7 +67,8 @@ if (isset($update['callback_query'])) {
 
     list($action, $event_id) = explode(":", $data);
 
-    $db->prepare("INSERT INTO responses (event_id, telegram_id, name) VALUES (?, ?, ?) ON DUPLICATE KEY UPDATE name = VALUES(name)") ->execute([$event_id, $user_id, $name]);
+    $db->prepare(" INSERT INTO responses (event_id, telegram_id, name) VALUES (?, ?, ?) ON DUPLICATE KEY UPDATE name = VALUES(name) ")->execute([$event_id, $user_id, $name]);// Přijdu
+    
     // Přijdu
     if ($action === "come_yes") {
         $db->prepare("UPDATE responses SET will_come = 1 WHERE telegram_id = ? AND event_id = ?")
