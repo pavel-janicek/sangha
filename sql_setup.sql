b288f1b0c3b783fdb3b789bbf6a7b71ab0d4f172
Pavel Janíček <pavel.janicek@live.com>
2026-01-27 09:45:50 +0100

    better db init script

--- sql_setup.sql
+++ sql_setup.sql
@@ -1,68 +1,30 @@
---
 -- Table structure for table `admins`
---
-
 CREATE TABLE `admins` (
-  `telegram_id` bigint(20) NOT NULL
+  `telegram_id` bigint(20) NOT NULL,
+  PRIMARY KEY (`telegram_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3 COLLATE=utf8mb3_czech_ci;
 
---
--- Indexes for table `admins`
---
-ALTER TABLE `admins`
-  ADD PRIMARY KEY (`telegram_id`);
-COMMIT;
 
---
 -- Table structure for table `events`
---
-
 CREATE TABLE `events` (
-  `id` int(11) NOT NULL,
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `title` varchar(255) DEFAULT NULL,
   `date` date DEFAULT NULL,
-  `is_active` tinyint(4) DEFAULT 1
+  `is_active` tinyint(4) DEFAULT 1,
+  PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3 COLLATE=utf8mb3_czech_ci;
 
---
--- Indexes for table `events`
---
-ALTER TABLE `events`
-  ADD PRIMARY KEY (`id`);
 
---
--- AUTO_INCREMENT for table `events`
---
-ALTER TABLE `events`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
-COMMIT;
-
---
 -- Table structure for table `responses`
---
-
 CREATE TABLE `responses` (
-  `id` int(11) NOT NULL,
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `event_id` int(11) DEFAULT NULL,
   `telegram_id` bigint(20) DEFAULT NULL,
   `name` varchar(255) DEFAULT NULL,
   `will_come` tinyint(4) DEFAULT NULL,
   `brings` text DEFAULT NULL,
   `does` text DEFAULT NULL,
-  `updated_at` timestamp NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
+  `updated_at` timestamp NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `event_user_unique` (`event_id`, `telegram_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3 COLLATE=utf8mb3_czech_ci;
-
---
--- Indexes for table `responses`
---
-ALTER TABLE `responses`
-  ADD PRIMARY KEY (`id`),
-  ADD UNIQUE KEY `event_id` (`event_id`,`telegram_id`),
-  ADD UNIQUE KEY `event_id_2` (`event_id`,`telegram_id`);
-
-  --
--- AUTO_INCREMENT for table `responses`
---
-ALTER TABLE `responses`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=41;
-COMMIT;
